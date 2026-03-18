2633f17010bf2a0a6dcf7fe90060999de37446bd
Sviat6239 <sviatoslav6239@gmail.com>
2026-03-18 18:04:48 +0100

    added data base for api

---
+++ db.js
@@ -0,0 +1,51 @@
+import Database from "better-sqlite3";
+
+const db = new Database("app.db");
+
+db.exec(`
+PRAGMA  foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS admins(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    login TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS customers(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    surname TEXT,
+    email TEXT,
+    phone TEXT
+);
+
+CREATE TABLE IF NOT EXISTS customers_links(
+    id INTEGER PRIMARY KEY AUTOINCREMET,
+    link TEXT NOT NULL,
+    customer_id INTEGER NOT NULL
+    FOREIGN KEY(customer_id) REFERENCES customers(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS services(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    service_id INTEGER,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS media(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER,
+    url TEXT NOT NULL,
+    type TEXT NOT NULL,
+    FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+`)
