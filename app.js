f9c75c37b587bc533a123cdfe7bd8a123bfb6bd0
Sviat6239 <sviatoslav6239@gmail.com>
2026-03-19 09:39:30 +0100

    added crud for customers

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const fastify = Fastify({
     logger: true
 })
 
-// crud for admins
+//CRUD for admins
 fastify.get('/api/admins', async (request, reply) => {
-    const stmt = db.prepare("SELECT * FROM products");
+    const stmt = db.prepare("SELECT * FROM admins");
     return stmt.all();
 });
 
@@ -18,13 +18,13 @@ fastify.get('/api/admins/:id', async (request, reply) => {
 
 fastify.post('/api/admin', async (request, reply) => {
     const { login, password } = req.body;
-    const stmt = db.prepare("INSERT INTO products (login, password) VALUES (?, ?)");
+    const stmt = db.prepare("INSERT INTO admins (login, password) VALUES (?, ?)");
     const info = stmt.run(login, password);
-    return { id: info.lasrInsertRowid };
+    return { id: info.lastInsertRowid };
 });
 
 fastify.patch('/api/admins/:id', async (request, reply) => {
-    const { login, password } = req.body:
+    const { login, password } = req.body;
     const stmt = db.prepare("UPDATE admins SET login = ?, password = ?");
     stmt.run(login, password, req.params.id);
     return { status: "ok" };
@@ -35,6 +35,37 @@ fastify.delete("/api/admins/:id", async (request, reply) => {
     stmt.run(req.params.id);
     return { status: "deleted" };
 })
+
+//CRUD for customers
+fastify.get('/api/customers', async (request, reply) => {
+    const stmt = db.prepare("SELECt * FROM customers");
+    return stmt.all();
+});
+
+fastify.get('/api/customers/:id', async (request, reply) => {
+    const stmt = db.prepare("SELECT * WHERE id = ?");
+    return stmt.get(req.params.id);
+})
+
+fastify.post('/api/customers', async (request, reply) => {
+    const { name, surname, email, phone } = req.body;
+    const stmt = db.prepare("INSERT INTO customers (name, surname, email, phone) VALUE(?, ?, ?, ?)");
+    const info = stmt.run(name, surname, email, phone);
+    return { id: info.lastInsertRowid };
+});
+
+fastify.patch('/api/customers/:id', async (request, reply) => {
+    const { name, surname, email, phone } = reply.body;
+    const stmt = db.prepare("UPDATE customers SET name = ?, surname = ?, email = ?, phone = ?");
+    stmt.run(name, surname, email, phone, req.params.id);
+    return { status: "ok" };
+});
+
+fastify.delete('/api/customers', async (request, reply) => {
+    const stmt = db.prepare("DELETE customers WHERE id = ?");
+    stmt.run(reply.params.id);
+    return { status: "deleted" };
+});
 
 const start = async () => {
     try {
